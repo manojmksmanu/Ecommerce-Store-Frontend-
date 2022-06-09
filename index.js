64db42f76513ee6df08aaf0c2f0867d7e6fc12f0
manojmksmanu <manoj2022019@gmail.com>
2022-06-09 21:24:37 +0530

    6

--- index.js
+++ index.js
@@ -18,13 +18,49 @@ close.addEventListener('click',() =>{
 // show.onclick = function(){
 //     location.href= "http://127.0.0.1:5501/singleproduct.html"
 // };
-var show=document.getElementById('pro');
-console.log(show);
-show.addEventListener('click', ()=> {
-    location.href= "http://127.0.0.1:5501/singleproduct.html"
+// var show=document.getElementById('pro');
+// console.log(show);
+// show.addEventListener('click', ()=> {
+//     location.href= "http://127.0.0.1:5501/singleproduct.html"
+// })
+
+///////////login page//////////
+
+var btn = document.getElementById('btn');
+var login_btn = document.getElementById('login-btn');
+var signup_btn = document.getElementById('signup-btn');
+var login_page= document.getElementById('login');
+var signup_page= document.getElementById('signup');
+var full_form = document.getElementById('final');
+var forgetbtn= document.getElementById('forget-pw');
+var forgetpage= document.getElementById('forget-page');
+var forget_cancel= document.getElementById('forget-cancel');
+
+forgetbtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+
+    forgetpage.style.display = 'block'
+    full_form.style.display = 'none';
+    
+})
+forget_cancel.addEventListener('click',()=>{
+    forgetpage.style.display = 'none'
+    full_form.style.display = 'block';
+   
 })
 
-
+signup_btn.addEventListener('click',()=>{
+    login_page.style.left= "-400px";
+    signup_page.style.left= "50px";
+    btn.style.left="150px";
+console.log('hello')
+})
+login_btn.addEventListener('click',()=>{
+    login_page.style.left= "50px";
+    signup_page.style.left= "400px";
+    btn.style.left="0px";
+console.log('hello')
+})
 
 
 ///////////Shop page javascript//////////////////
